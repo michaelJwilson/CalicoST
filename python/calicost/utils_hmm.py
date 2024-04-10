7cc12a60faf6c07d56665e6b3c0a698674f108c2
Michael J. Wilson <mw9568@princeton.edu>
2024-04-10 17:52:15 -0400

    fix

--- python/calicost/utils_hmm.py
+++ python/calicost/utils_hmm.py
@@ -1076,7 +1076,7 @@ def update_emission_params_nb_nophasing_uniqvalues_mix(unique_values, mapping_ma
     new_log_mu[new_log_mu < min_log_rdr] = min_log_rdr
     return new_log_mu, new_alphas
 
-
+@profile
 def update_emission_params_bb_nophasing_uniqvalues(unique_values, mapping_matrices, log_gamma, taus, \
     start_p_binom=None, fix_BB_dispersion=False, shared_BB_dispersion=False, \
     percent_threshold=0.99, min_binom_prob=0.01, max_binom_prob=0.99):
