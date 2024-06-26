70b3d565ef8f8aa37023bdbd881b269dce379720
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-06-25 17:05:09 -0700

    fix

--- src/calicost/hmm_NB_BB_nophasing_v2.py
+++ src/calicost/hmm_NB_BB_nophasing_v2.py
@@ -72,32 +72,6 @@ class hmm_nophasing_v2(object):
 
         return compute_emission_probability_nb_betabinom(X, base_nb_mean, log_mu, alphas, total_bb_RD, p_binom, taus)
         
-        """
-        n_states = log_mu.shape[0]
-        (n_obs, n_comp, n_spots) = X.shape
-
-        log_emission_rdr = np.zeros((n_states, n_obs, n_spots))
-        log_emission_baf = np.zeros((n_states, n_obs, n_spots))
-        
-        for i in np.arange(n_states):
-            for s in np.arange(n_spots):
-                idx_nonzero_rdr = np.where(base_nb_mean[:,s] > 0)[0]
-                idx_nonzero_baf = np.where(total_bb_RD[:,s] > 0)[0]
-                
-                if len(idx_nonzero_rdr) > 0:
-                    nb_mean = base_nb_mean[idx_nonzero_rdr,s] * np.exp(log_mu[i, s])
-                    nb_std = np.sqrt(nb_mean + alphas[i, s] * nb_mean**2)
-                    
-                    n, p = convert_params(nb_mean, nb_std)
-                    
-                    log_emission_rdr[i, idx_nonzero_rdr, s] = scipy.stats.nbinom.logpmf(X[idx_nonzero_rdr, 0, s], n, p)
-                
-                if len(idx_nonzero_baf) > 0:
-                    log_emission_baf[i, idx_nonzero_baf, s] = scipy.stats.betabinom.logpmf(X[idx_nonzero_baf,1,s], total_bb_RD[idx_nonzero_baf,s], p_binom[i, s] * taus[i, s], (1-p_binom[i, s]) * taus[i, s])
-                    
-        return log_emission_rdr, log_emission_baf
-        """
-        
     @staticmethod
     def compute_emission_probability_nb_betabinom_mix(X, base_nb_mean, log_mu, alphas, total_bb_RD, p_binom, taus, tumor_prop, **kwargs):
         """
@@ -238,7 +212,7 @@ class hmm_nophasing_v2(object):
             cumlen += le
         return log_beta
 
-    #
+    @profile
     def run_baum_welch_nb_bb(self, X, lengths, n_states, base_nb_mean, total_bb_RD, log_sitewise_transmat=None, tumor_prop=None, \
         fix_NB_dispersion=False, shared_NB_dispersion=False, fix_BB_dispersion=False, shared_BB_dispersion=False, \
         is_diag=False, init_log_mu=None, init_p_binom=None, init_alphas=None, init_taus=None, max_iter=100, tol=1e-4, **kwargs):
