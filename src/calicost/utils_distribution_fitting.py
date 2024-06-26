ef5b9ec207b00538e68320f8f7f0fa39b2bd32b2
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-06-25 17:21:13 -0700

    fix

--- src/calicost/utils_distribution_fitting.py
+++ src/calicost/utils_distribution_fitting.py
@@ -57,30 +57,33 @@ class Weighted_NegativeBinomial(GenericLikelihoodModel):
     """
     def __init__(self, endog, exog, weights, exposure, seed=0, **kwds):
         super(Weighted_NegativeBinomial, self).__init__(endog, exog, **kwds)
+        
         self.weights = weights
         self.exposure = exposure
         self.seed = seed
-    #
+
     def nloglikeobs(self, params):
         nb_mean = np.exp(self.exog @ params[:-1]) * self.exposure
         nb_std = np.sqrt(nb_mean + params[-1] * nb_mean**2)
+
         n, p = convert_params(nb_mean, nb_std)
+
         llf = scipy.stats.nbinom.logpmf(self.endog, n, p)
-        neg_sum_llf = -llf.dot(self.weights)
-        return neg_sum_llf
-    #
+
+        return -llf.dot(self.weights)
+
     def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
         self.exog_names.append('alpha')
+        
         if start_params is None:
             if hasattr(self, 'start_params'):
                 start_params = self.start_params
             else:
-                start_params = np.append(0.1 * np.ones(self.nparams), 0.01)
+                start_params = np.append(0.1 * np.ones(self.nparams), 1.e-2)
 
         return super(Weighted_NegativeBinomial, self).fit(start_params=start_params,
-                                                          maxiter=maxiter, maxfun=maxfun, method="bfgs",
+                                                          maxiter=maxiter, maxfun=maxfun,
                                                           **kwds)
-
 
 class Weighted_NegativeBinomial_mix(GenericLikelihoodModel):
     def __init__(self, endog, exog, weights, exposure, tumor_prop, seed=0, **kwds):
@@ -89,15 +92,19 @@ class Weighted_NegativeBinomial_mix(GenericLikelihoodModel):
         self.exposure = exposure
         self.seed = seed
         self.tumor_prop = tumor_prop
-    #
+
     def nloglikeobs(self, params):
         nb_mean = self.exposure * (self.tumor_prop * np.exp(self.exog @ params[:-1]) + 1 - self.tumor_prop)
         nb_std = np.sqrt(nb_mean + params[-1] * nb_mean**2)
+        
         n, p = convert_params(nb_mean, nb_std)
+        
         llf = scipy.stats.nbinom.logpmf(self.endog, n, p)
+        
         neg_sum_llf = -llf.dot(self.weights)
+        
         return neg_sum_llf
-    #
+
     def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
         self.exog_names.append('alpha')
         if start_params is None:
@@ -133,14 +140,15 @@ class Weighted_BetaBinom(GenericLikelihoodModel):
         super(Weighted_BetaBinom, self).__init__(endog, exog, **kwds)
         self.weights = weights
         self.exposure = exposure
-    #
+
     def nloglikeobs(self, params):
         a = (self.exog @ params[:-1]) * params[-1]
-        b = (1 - self.exog @ params[:-1]) * params[-1]
+        b = (1. - self.exog @ params[:-1]) * params[-1]
+        
         llf = scipy.stats.betabinom.logpmf(self.endog, self.exposure, a, b)
-        neg_sum_llf = -llf.dot(self.weights)
-        return neg_sum_llf
-    #
+        
+        return -llf.dot(self.weights)
+
     def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
         self.exog_names.append("tau")
         if start_params is None:
@@ -148,9 +156,10 @@ class Weighted_BetaBinom(GenericLikelihoodModel):
                 start_params = self.start_params
             else:
                 start_params = np.append(0.5 / np.sum(self.exog.shape[1]) * np.ones(self.nparams), 1)
+                
         return super(Weighted_BetaBinom, self).fit(start_params=start_params,
-                                               maxiter=maxiter, maxfun=maxfun,
-                                               **kwds)
+                                                   maxiter=maxiter, maxfun=maxfun,
+                                                   **kwds)
 
 
 class Weighted_BetaBinom_mix(GenericLikelihoodModel):
