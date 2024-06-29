be460e2664b4d9bc873b011626835766000f4b45
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-06-28 21:31:15 -0700

    fix

--- src/calicost/hmm_NB_BB_nophasing_v2.py
+++ src/calicost/hmm_NB_BB_nophasing_v2.py
@@ -22,6 +22,12 @@ Joint NB-BB HMM that accounts for tumor/normal genome proportions. Tumor genome 
 # whole inference
 ############################################################
 
+logger = logging.getLogger(__name__)
+
+def convergence(new, old, tol):
+    result = np.mean(np.abs( np.exp(new) - np.exp(old) ))
+    return result, result < tol
+
 class hmm_nophasing_v2(object):
     def __init__(self, params="stmp", t=1-1e-4):
         """
@@ -291,7 +297,7 @@ class hmm_nophasing_v2(object):
     def run_baum_welch_nb_bb(self, X, lengths, n_states, base_nb_mean, total_bb_RD, log_sitewise_transmat=None, tumor_prop=None, \
         fix_NB_dispersion=False, shared_NB_dispersion=False, fix_BB_dispersion=False, shared_BB_dispersion=False, \
         is_diag=False, init_log_mu=None, init_p_binom=None, init_alphas=None, init_taus=None, max_iter=100, tol=1e-4, **kwargs):
-        '''
+        """
         Input
             X: size n_observations * n_components * n_spots.
             lengths: sum of lengths = n_observations.
@@ -300,33 +306,41 @@ class hmm_nophasing_v2(object):
         Intermediate
             log_mu: size of n_states. Log of mean/exposure/base_prob of each HMM state.
             alpha: size of n_states. Dispersioon parameter of each HMM state.
-        '''
-        n_obs = X.shape[0]
-        n_comp = X.shape[1]
-        n_spots = X.shape[2]
+        """
+        n_obs, n_comp, n_spots  = X.shape
+
         assert n_comp == 2
+        
         # initialize NB logmean shift and BetaBinom prob
         log_mu = np.vstack([np.linspace(-0.1, 0.1, n_states) for r in range(n_spots)]).T if init_log_mu is None else init_log_mu
         p_binom = np.vstack([np.linspace(0.05, 0.45, n_states) for r in range(n_spots)]).T if init_p_binom is None else init_p_binom
+        
         # initialize (inverse of) dispersion param in NB and BetaBinom
         alphas = 0.1 * np.ones((n_states, n_spots)) if init_alphas is None else init_alphas
-        taus = 30 * np.ones((n_states, n_spots)) if init_taus is None else init_taus
+        taus = 30. * np.ones((n_states, n_spots)) if init_taus is None else init_taus
+        
         # initialize start probability and emission probability
         log_startprob = np.log( np.ones(n_states) / n_states )
+        
         if n_states > 1:
             transmat = np.ones((n_states, n_states)) * (1-self.t) / (n_states-1)
             np.fill_diagonal(transmat, self.t)
             log_transmat = np.log(transmat)
         else:
             log_transmat = np.zeros((1,1))
+            
         # initialize log_gamma
         log_gamma = kwargs["log_gamma"] if "log_gamma" in kwargs else None
+
+        logger.info(f"Baum-Welch initialization complete. log_mu=\n{log_mu}; p_binom=\n{p_binom}; alphas=\n{alphas}; taus=\n{taus}")
+        
         # a trick to speed up BetaBinom optimization: taking only unique values of (B allele count, total SNP covering read count)
         unique_values_nb, mapping_matrices_nb = construct_unique_matrix(X[:,0,:], base_nb_mean)
         unique_values_bb, mapping_matrices_bb = construct_unique_matrix(X[:,1,:], total_bb_RD)
+        
         # EM algorithm
-        for r in trange(max_iter):
-            # E step
+        for r in trange(max_iter, desc="EM algorithm"):
+            # E step: infer copy number states
             if tumor_prop is None:
                 log_emission_rdr, log_emission_baf = hmm_nophasing_v2.compute_emission_probability_nb_betabinom(X, base_nb_mean, log_mu, alphas, total_bb_RD, p_binom, taus)
                 log_emission = log_emission_rdr + log_emission_baf
@@ -334,28 +348,30 @@ class hmm_nophasing_v2(object):
                 # compute mu as adjusted RDR
                 if ((not log_gamma is None) or (r > 0)) and ("m" in self.params):
                     logmu_shift = []
+                    
                     for c in range(len(kwargs["sample_length"])):
                         this_pred_cnv = np.argmax(log_gamma[:,np.sum(kwargs["sample_length"][:c]):np.sum(kwargs["sample_length"][:(c+1)])], axis=0)%n_states
                         logmu_shift.append( scipy.special.logsumexp(log_mu[this_pred_cnv,:] + np.log(kwargs["lambd"]).reshape(-1,1), axis=0) )
                     logmu_shift = np.vstack(logmu_shift)
-                    log_emission_rdr, log_emission_baf = hmm_nophasing_v2.compute_emission_probability_nb_betabinom_mix(X, base_nb_mean, log_mu, alphas, total_bb_RD, p_binom, taus, tumor_prop, logmu_shift=logmu_shift, sample_length=kwargs["sample_length"])
+                    log_emission_rdr, log_emission_baf = hmm_nophasing_v2.compute_emission_probability_nb_betabinom_mix(
+                        X, base_nb_mean, log_mu, alphas, total_bb_RD, p_binom, taus, tumor_prop, logmu_shift=logmu_shift, sample_length=kwargs["sample_length"]
+                    )
                 else:
-                    log_emission_rdr, log_emission_baf = hmm_nophasing_v2.compute_emission_probability_nb_betabinom_mix(X, base_nb_mean, log_mu, alphas, total_bb_RD, p_binom, taus, tumor_prop)
+                    log_emission_rdr, log_emission_baf = hmm_nophasing_v2.compute_emission_probability_nb_betabinom_mix(
+                        X, base_nb_mean, log_mu, alphas, total_bb_RD, p_binom, taus, tumor_prop
+                    )
+                    
                 log_emission = log_emission_rdr + log_emission_baf
+                
             log_alpha = hmm_nophasing_v2.forward_lattice(lengths, log_transmat, log_startprob, log_emission, log_sitewise_transmat)
             log_beta = hmm_nophasing_v2.backward_lattice(lengths, log_transmat, log_startprob, log_emission, log_sitewise_transmat)
             log_gamma = compute_posterior_obs(log_alpha, log_beta)
             log_xi = compute_posterior_transition_nophasing(log_alpha, log_beta, log_transmat, log_emission)
-            # M step
-            if "s" in self.params:
-                new_log_startprob = update_startprob_nophasing(lengths, log_gamma)
-                new_log_startprob = new_log_startprob.flatten()
-            else:
-                new_log_startprob = log_startprob
-            if "t" in self.params:
-                new_log_transmat = update_transition_nophasing(log_xi, is_diag=is_diag)
-            else:
-                new_log_transmat = log_transmat
+            
+            # M step: update params.
+            new_log_startprob = update_startprob_nophasing(lengths, log_gamma).flatten() if "s" in self.params else log_startprob
+            new_log_transmat = update_transition_nophasing(log_xi, is_diag=is_diag) if "t" in self.params else log_transmat
+                
             if "m" in self.params:
                 if tumor_prop is None:
                     new_log_mu, new_alphas = update_emission_params_nb_nophasing_uniqvalues(unique_values_nb, mapping_matrices_nb, log_gamma, alphas, start_log_mu=log_mu, \
@@ -366,10 +382,13 @@ class hmm_nophasing_v2(object):
             else:
                 new_log_mu = log_mu
                 new_alphas = alphas
+                
             if "p" in self.params:
                 if tumor_prop is None:
-                    new_p_binom, new_taus = update_emission_params_bb_nophasing_uniqvalues(unique_values_bb, mapping_matrices_bb, log_gamma, taus, start_p_binom=p_binom, \
-                        fix_BB_dispersion=fix_BB_dispersion, shared_BB_dispersion=shared_BB_dispersion)
+                    new_p_binom, new_taus = update_emission_params_bb_nophasing_uniqvalues(
+                        unique_values_bb, mapping_matrices_bb, log_gamma, taus, start_p_binom=p_binom, \
+                        fix_BB_dispersion=fix_BB_dispersion, shared_BB_dispersion=shared_BB_dispersion
+                    )
                 else:
                     # compute mu as adjusted RDR
                     if ("m" in self.params):
@@ -381,26 +400,42 @@ class hmm_nophasing_v2(object):
                         weighted_tp = (tumor_prop * mu) / (tumor_prop * mu + 1 - tumor_prop)
                     else:
                         weighted_tp = tumor_prop
-                    new_p_binom, new_taus = update_emission_params_bb_nophasing_uniqvalues_mix(unique_values_bb, mapping_matrices_bb, log_gamma, taus, weighted_tp, start_p_binom=p_binom, \
-                        fix_BB_dispersion=fix_BB_dispersion, shared_BB_dispersion=shared_BB_dispersion)
+                        
+                    new_p_binom, new_taus = update_emission_params_bb_nophasing_uniqvalues_mix(
+                        unique_values_bb, mapping_matrices_bb, log_gamma, taus, weighted_tp, start_p_binom=p_binom, \
+                        fix_BB_dispersion=fix_BB_dispersion, shared_BB_dispersion=shared_BB_dispersion
+                    )
             else:
                 new_p_binom = p_binom
                 new_taus = taus
+                
             # check convergence
-            print( np.mean(np.abs( np.exp(new_log_startprob) - np.exp(log_startprob) )), \
-                np.mean(np.abs( np.exp(new_log_transmat) - np.exp(log_transmat) )), \
-                np.mean(np.abs(new_log_mu - log_mu)),\
-                np.mean(np.abs(new_p_binom - p_binom)) )
-            print( np.hstack([new_log_mu, new_p_binom]) )
-            if np.mean(np.abs( np.exp(new_log_transmat) - np.exp(log_transmat) )) < tol and \
-                np.mean(np.abs(new_log_mu - log_mu)) < tol and np.mean(np.abs(new_p_binom - p_binom)) < tol:
+            names = ["log_startprob", "log_transmat", "log_mu", "p_binom"]
+            old_arrays = (log_startprob, log_transmat, log_mu, np.log(p_binom))
+            new_arrays = (new_log_startprob, new_log_transmat, new_log_mu, np.log(new_p_binom))
+            
+            for name, new, old in zip(names, new_arrays, old_arrays):
+                result = convergence(new, old, tol)
+
+                # logger.info(f"\n{name}\n{new}\n{old}\n{interim}")
+                logger.info(f"EM convergence ({tol:.3e} tol): {name} diff.\t{result[0]:.6e}\t{result[1]}")
+                
+            # TODO BUG? exp mu.
+            is_converged = np.mean(np.abs( np.exp(new_log_transmat) - np.exp(log_transmat) )) < tol
+            is_converged = is_converged and np.mean(np.abs( np.exp(new_log_mu) - np.exp(log_mu))) < tol
+            is_converged = is_converged and np.mean(np.abs(new_p_binom - p_binom)) < tol
+            
+            if is_converged:
                 break
-            log_startprob = new_log_startprob
-            log_transmat = new_log_transmat
-            log_mu = new_log_mu
-            alphas = new_alphas
+            
+            log_startprob, log_transmat, log_mu = new_log_startprob, new_log_transmat, new_log_mu
+
+            alphas, taus = new_alphas, new_taus
             p_binom = new_p_binom
-            taus = new_taus
+
+        for name, new in zip(names, new_arrays):
+            logger.info(f"EM complete ({tol:.6e} tol): {name}\t{new}")
+            
         return new_log_mu, new_alphas, new_p_binom, new_taus, new_log_startprob, new_log_transmat, log_gamma
 
 
