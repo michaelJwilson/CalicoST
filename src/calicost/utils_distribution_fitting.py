1c96ccda30e4a7710a75e73a98f3e5bcc4c479ad
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-07-18 13:45:16 +0100

    fix

--- src/calicost/utils_distribution_fitting.py
+++ src/calicost/utils_distribution_fitting.py
@@ -103,29 +103,44 @@ class Weighted_NegativeBinomial(GenericLikelihoodModel):
 class Weighted_NegativeBinomial_mix(GenericLikelihoodModel):
     def __init__(self, endog, exog, weights, exposure, tumor_prop, seed=0, **kwds):
         super(Weighted_NegativeBinomial_mix, self).__init__(endog, exog, **kwds)
+        
         self.weights = weights
         self.exposure = exposure
         self.seed = seed
         self.tumor_prop = tumor_prop
 
-    def nloglikeobs(self, params):
-        nb_mean = self.exposure * (self.tumor_prop * np.exp(self.exog @ params[:-1]) + 1 - self.tumor_prop)
-        nb_std = np.sqrt(nb_mean + params[-1] * nb_mean**2)
-        n, p = convert_params(nb_mean, nb_std)
-        llf = scipy.stats.nbinom.logpmf(self.endog, n, p)
-        neg_sum_llf = -llf.dot(self.weights)
-        return neg_sum_llf
+        self.n_spots = len(tumor_prop)
+        self.n_states = exog.shape[1]
 
+        # self.nloglikeobs_zeropoint = self.exposure * (1. - self.tumor_prop)   
+        
+        logger.info(f"Fitting Weighted_NegativeBinomial_mix for {self.n_spots} spots and {self.n_states} states.")
+        
+    @profile
+    def nloglikeobs(self, params):
+        nb_mean = self.exposure * (self.tumor_prop * (self.exog @ np.exp(params[:-1])) + 1. - self.tumor_prop)
+        nb_var = nb_mean + params[-1] * nb_mean**2
+        
+        n, p = convert_params_var(nb_mean, nb_var)
+
+        # return -scipy.stats.nbinom.logpmf(self.endog, n, p).dot(self.weights)
+        return -calicostem.nb(self.endog.astype(float), n.astype(float), p).dot(self.weights)
+    
     def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
         self.exog_names.append('alpha')
+        
         if start_params is None:
             if hasattr(self, 'start_params'):
                 start_params = self.start_params
             else:
                 start_params = np.append(0.1 * np.ones(self.nparams), 0.01)
-        return super(Weighted_NegativeBinomial_mix, self).fit(start_params=start_params,
-                                               maxiter=maxiter, maxfun=maxfun,
-                                               **kwds)
+                
+        return super(Weighted_NegativeBinomial_mix, self).fit(
+            start_params=start_params,
+            maxiter=maxiter,
+            maxfun=maxfun,
+            **kwds
+        )
 
 
 class Weighted_BetaBinom(GenericLikelihoodModel):
@@ -184,7 +199,8 @@ class Weighted_BetaBinom_mix(GenericLikelihoodModel):
         
         self.weights = weights
         self.tumor_prop = tumor_prop
-
+        self.exposure = exposure
+        
         self.n_spots = len(tumor_prop)
         self.n_states = exog.shape[1]
 
