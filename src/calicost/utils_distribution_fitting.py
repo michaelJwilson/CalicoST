569f5d1bfc59f77fb79ca3316a0571faa3f056c4
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-07-18 10:43:34 +0100

    3.3x speedup in Weighted BetaBinom mix for 5k spots.

--- src/calicost/utils_distribution_fitting.py
+++ src/calicost/utils_distribution_fitting.py
@@ -179,6 +179,7 @@ class Weighted_BetaBinom(GenericLikelihoodModel):
 class Weighted_BetaBinom_mix(GenericLikelihoodModel):
     def __init__(self, endog, exog, weights, exposure, tumor_prop, **kwds):
         super(Weighted_BetaBinom_mix, self).__init__(endog, exog, **kwds)
+        
         self.weights = weights
         self.exposure = exposure
         self.tumor_prop = tumor_prop
@@ -186,21 +187,27 @@ class Weighted_BetaBinom_mix(GenericLikelihoodModel):
     @profile
     def nloglikeobs(self, params):
         a = (self.exog @ params[:-1] * self.tumor_prop + 0.5 * (1 - self.tumor_prop)) * params[-1]
-        b = ((1 - self.exog @ params[:-1]) * self.tumor_prop + 0.5 * (1 - self.tumor_prop)) * params[-1]
-        llf = scipy.stats.betabinom.logpmf(self.endog, self.exposure, a, b)
-        neg_sum_llf = -llf.dot(self.weights)
-        return neg_sum_llf
+        b = ((self.exog @ (1. - params[:-1])) * self.tumor_prop + 0.5 * (1 - self.tumor_prop)) * params[-1]
 
-    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+        # NB negative sum log likelihood.
+        # return -scipy.stats.betabinom.logpmf(self.endog, self.exposure, a, b).dot(self.weights)
+        return -calicostem.bb(self.endog.astype(float), self.exposure.astype(float), a, b).dot(self.weights)
+        
+    def fit(self, start_params=None, maxiter=10_000, maxfun=5_000, **kwds):
         self.exog_names.append("tau")
+        
         if start_params is None:
             if hasattr(self, 'start_params'):
                 start_params = self.start_params
             else:
                 start_params = np.append(0.5 / np.sum(self.exog.shape[1]) * np.ones(self.nparams), 1)
-        return super(Weighted_BetaBinom_mix, self).fit(start_params=start_params,
-                                               maxiter=maxiter, maxfun=maxfun,
-                                               **kwds)
+                
+        return super(Weighted_BetaBinom_mix, self).fit(
+            start_params=start_params,
+            maxiter=maxiter,
+            maxfun=maxfun,
+            **kwds
+        )
 
 
 class Weighted_BetaBinom_fixdispersion(GenericLikelihoodModel):
