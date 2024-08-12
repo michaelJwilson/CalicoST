293ddc659efde35e18903591c0eae478410c8b7d
Michael J. Wilson <mw9568@princeton.edu>
2024-08-12 09:47:23 -0400

    fix

--- src/calicost/utils_distribution_fitting.py
+++ src/calicost/utils_distribution_fitting.py
@@ -58,7 +58,7 @@ class Weighted_NegativeBinomial(GenericLikelihoodModel):
         self.exposure = exposure
         self.seed = seed
 
-    @profile
+    # @profile
     def nloglikeobs(self, params):
         nb_mean = np.exp(self.exog @ params[:-1]) * self.exposure
         nb_std = np.sqrt(nb_mean + params[-1] * nb_mean**2)
@@ -132,7 +132,7 @@ class Weighted_BetaBinom(GenericLikelihoodModel):
         self.weights = weights
         self.exposure = exposure
 
-    @profile
+    # @profile
     def nloglikeobs(self, params):
         a = (self.exog @ params[:-1]) * params[-1]
         b = (1 - self.exog @ params[:-1]) * params[-1]
@@ -159,7 +159,7 @@ class Weighted_BetaBinom_mix(GenericLikelihoodModel):
         self.exposure = exposure
         self.tumor_prop = tumor_prop
 
-    @profile
+    # @profile
     def nloglikeobs(self, params):
         a = (self.exog @ params[:-1] * self.tumor_prop + 0.5 * (1 - self.tumor_prop)) * params[-1]
         b = ((1 - self.exog @ params[:-1]) * self.tumor_prop + 0.5 * (1 - self.tumor_prop)) * params[-1]
@@ -186,7 +186,7 @@ class Weighted_BetaBinom_fixdispersion(GenericLikelihoodModel):
         self.weights = weights
         self.exposure = exposure
 
-    @profile
+    # @profile
     def nloglikeobs(self, params):
         a = (self.exog @ params) * self.tau
         b = (1 - self.exog @ params) * self.tau
@@ -214,7 +214,7 @@ class Weighted_BetaBinom_fixdispersion_mix(GenericLikelihoodModel):
         self.exposure = exposure
         self.tumor_prop = tumor_prop
 
-    @profile
+    # @profile
     def nloglikeobs(self, params):
         a = (self.exog @ params * self.tumor_prop + 0.5 * (1 - self.tumor_prop)) * self.tau
         b = ((1 - self.exog @ params) * self.tumor_prop + 0.5 * (1 - self.tumor_prop)) * self.tau
